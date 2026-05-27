88e84548fea833d2e8fb66c46e4041e60d14d57b
JuanDiego <renteriadiego639@gmail.com>
2026-05-27 09:55:16 -0500

    listo para render

--- app.py
+++ app.py
@@ -30,4 +30,8 @@ def inicio():
     return render_template('index.html', productos=productos)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
